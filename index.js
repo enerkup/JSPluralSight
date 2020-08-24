5664d2622d2dd1c56abf42307336bdbcad48b599
Ernesto Cárdenas <distritomx@gmail.com>
2020-08-23 22:00:36 -0500

    Modern JavaScript - Classes

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-var demo = 3;
+
+//Classes 
+
+class Persona {
+    constructor(name) {
+        this.name =  name;
+    }
+    hi() {
+        console.log(`Hi ${this.name}`);
+    }
+}
+
+class Estudiante extends Persona {
+    constructor(name,level) {
+        super(name);
+        this.level = level;
+    }
+
+    hi() {
+        console.log(`Hi ${this.name} grade ${this.level}`);
+    }
+
+}
+
+const p1 = new Persona("Jame");
+const p2 = new Estudiante("Jina","Bech 02");
+
+p1.hi = () => console.log('Salta saludo');
+
+p1.hi();
+p2.hi();
